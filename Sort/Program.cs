69fbf06000bde1e09d2ec95bc1e2123ac5b45378
Matthew Brady <matt_brad@outlook.com>
2017-11-16 21:59:03 -0500

    tree sort done and tested

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -8,24 +8,31 @@ namespace Sort
 {
     class Program
     {
+        public static int[] numbers = new int[10];
         static void Main(string[] args)
         {
             build_array();
+            send_array();
         }
         public static void build_array()
         {
-            int[] numbers = new int[10];            
+                        
             Random random = new Random();
             for(int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = random.Next(1, 100);
                 
-            }
-            foreach(int n in numbers)
-            {
-                Console.WriteLine(n);
-            }
-            Console.Read();
+            }            
         }
+        public static void send_array()
+        {
+            Binary_tree_sort tree = new Binary_tree_sort();
+            foreach (int n in numbers)
+            {
+                tree.get_array(n);
+            }
+            tree.sort();
+        }
+
     }
 }
