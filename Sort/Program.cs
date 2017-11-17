6c2b35a64c16f947277700badcdb157feab36ec0
Matthew Brady <matt_brad@outlook.com>
2017-11-16 20:18:24 -0500

    Add project files.

---
+++ Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            build_array();
+        }
+        public static void build_array()
+        {
+            int[] numbers = new int[10];            
+            Random random = new Random();
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 100);
+                
+            }
+            foreach(int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+            Console.Read();
+        }
+    }
+}
